a4d4368fd2eb127944daf9b6480923f9a50537fc
Gabriel Dantas <gabrieldantaslp@gmail.com>
2026-03-03 14:09:17 -0300

    Operações CRUD assíncronas em APIs RESTful

--- apis-assincronas-com-fastapi/dio-blog/controllers/post.py
+++ apis-assincronas-com-fastapi/dio-blog/controllers/post.py
@@ -1,20 +1,25 @@
+from fastapi import APIRouter, status
 
-from fastapi import  Response, status,  APIRouter
-
-from schemas.post import PostIn
+from schemas.post import PostIn, PostUpdateIn
 from views.post import PostOut
+from models.post import posts
+from database import database
 
 router = APIRouter(prefix="/posts")
-  
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=PostIn)
-def create_post(post: PostIn):
-  return post
 
-@router.get('/', response_model=list[PostOut])
-def read_posts(
-  response: Response, 
-  published: bool, 
-  limit: int, 
-  skip: int = 0, 
-):
-  return []
+@router.get("/", response_model=list[PostOut])
+async def read_posts(published: bool, limit: int, skip: int = 0):
+    query = posts.select()
+    return await database.fetch_all(query)
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
+async def create_post(post: PostIn):
+    query = posts.insert().values(
+        title=post.title, 
+        content=post.content, 
+        published_at=post.published_at, 
+        published=post.published
+    )
+    last_id = await database.execute(query)
+    return {**post.model_dump(), "id": last_id}
+
