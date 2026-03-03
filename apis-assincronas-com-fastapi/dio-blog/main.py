3c64bec9450abc6745748a9dcafc2222ad441808
Gabriel Dantas <gabrieldantaslp@gmail.com>
2026-03-03 10:17:36 -0300

    Rotas e endpoints em FastAPI - Request body

--- apis-assincronas-com-fastapi/dio-blog/main.py
+++ apis-assincronas-com-fastapi/dio-blog/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from datetime import UTC, datetime
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,11 +10,24 @@ fake_db = [
     { 'title': 'Arquitetura limpa com FastAPI', 'published': False },
     { 'title': 'Criando uma aplicação com Spring Boot', 'published': True },
     { 'title': 'Arquitetura limpa com Spring Boot', 'published': True },
-  ]
+]
 
-@app.get('/posts')
-def read_posts(published: bool, skip: int = 0, limit: int = len(fake_db)):
-  return [post for post in fake_db[skip: skip + limit] if post['published'] is published]
+class Post(BaseModel):
+  title: str
+  date: datetime = datetime.now(UTC)
+  published: bool = False
+  
+@app.post('/posts/', status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+  fake_db.append(post.model_dump())
+  return post
+
+@app.get('/posts/')
+def read_posts(published: bool, limit: int, skip: int = 0):
+  return [
+    post for post in fake_db[skip : skip + limit] 
+    if post['published'] is published
+  ]
 
 @app.get('/posts/{framework}')
 def read_framework_posts(framework: str):
